7067ce57e4e0e7ee452d2042aefc1b7a8c224a55
Noe <VaescoNoe@gmail.com>
2019-03-18 22:37:11 -0600

    se integra la capa da datos

--- src/main/java/mx/com/vargas/sga/domain/Persona.java
+++ src/main/java/mx/com/vargas/sga/domain/Persona.java
@@ -2,6 +2,18 @@ package mx.com.vargas.sga.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Entity
+@NamedQueries({@NamedQuery(name="Persona.findAll",query="SELECT p FROM Persona p ORDER BY p.idPersona")})
+@Table(name="persona")
 public class Persona implements Serializable {
 
 	/**
@@ -9,11 +21,24 @@ public class Persona implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id_persona")
 	private int idPersona;
+	
+	@Column(nullable = false,length=50)
 	private String nombre;
+	
+	@Column(name="apellido_paterno",nullable = false,length=50)
 	private String apePaterno;
+	
+	@Column(name="apellido_materno",length=50)
 	private String apeMaterno;
+	
+	@Column(nullable = false,length=50)
 	private String email;
+	
+	@Column(nullable = false)
 	private String telefono;
 
 	public Persona() {
@@ -23,6 +48,15 @@ public class Persona implements Serializable {
 	public Persona(int idPersona, String nombre, String apePaterno, String apeMaterno, String email, String telefono) {
 		super();
 		this.idPersona = idPersona;
+		this.nombre = nombre;
+		this.apePaterno = apePaterno;
+		this.apeMaterno = apeMaterno;
+		this.email = email;
+		this.telefono = telefono;
+	}
+	
+	public Persona( String nombre, String apePaterno, String apeMaterno, String email, String telefono) {
+		super();
 		this.nombre = nombre;
 		this.apePaterno = apePaterno;
 		this.apeMaterno = apeMaterno;
--- src/main/java/mx/com/vargas/sga/servicio/PersonaServiceRemoteImpl.java
+++ src/main/java/mx/com/vargas/sga/servicio/PersonaServiceRemoteImpl.java
@@ -3,45 +3,40 @@ package mx.com.vargas.sga.servicio;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
 import mx.com.vargas.sga.domain.Persona;
+import mx.com.vargas.sga.eis.PersonaDao;
 
 @Stateless
-public class PersonaServiceRemoteImpl implements PersonaServiceRemote,PersonaService {
+public class PersonaServiceRemoteImpl implements PersonaServiceRemote, PersonaService {
+
+	@EJB
+	private PersonaDao personaDao;
 
 	public List<Persona> listarPersonas() {
-		List<Persona> personas = new ArrayList<Persona>();
-		
-		personas.add(new Persona(1, "Noe", "Vargas", "Escorcia", "vargas@gmail.com", "12345678"));
-		personas.add(new Persona(2, "Paulina", "Pastrana", "Guzman", "pastrana@gmail.com", "87654321"));
-		
-		return personas;
+		return personaDao.findAllPersonas();
 	}
 
 	public Persona encontrarPersonaPorId(Persona persona) {
-		// TODO Auto-generated method stub
-		return null;
+		return personaDao.findPersonaById(persona);
 	}
 
 	public Persona encontrarPersonaPorEmail(Persona persona) {
-		// TODO Auto-generated method stub
-		return null;
+		return personaDao.findPersonaByEmail(persona);
 	}
 
 	public void registrarPersona(Persona persona) {
-		// TODO Auto-generated method stub
-
+		personaDao.insertPersona(persona);
 	}
 
 	public void modificarPersona(Persona persona) {
-		// TODO Auto-generated method stub
-
+		personaDao.updatePersona(persona);
 	}
 
 	public void eliminarPersona(Persona persona) {
-		// TODO Auto-generated method stub
-
+		personaDao.deletePersona(persona);
 	}
 
 }
